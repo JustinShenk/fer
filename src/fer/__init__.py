661b7a0a79a042238b7852543512e24910116be5
Justin Shenk <shenkjustin@gmail.com>
2019-07-21 17:15:28 +0200

    Bump version

--- src/fer/__init__.py
+++ src/fer/__init__.py
@@ -26,7 +26,7 @@
 from fer.fer import FER
 from fer.classes import Video
 
-__version__ = "19.0.5.dev2"
+__version__ = "19.0.6"
 
 __title__ = "fer"
 __description__ = "Facial expression recognition from images"
