84b74e56bbaf58be12ccdebac3928c52ced786cd
Justin Shenk <shenkjustin@gmail.com>
2020-11-10 09:57:30 +0100

    Update version

--- src/fer/__init__.py
+++ src/fer/__init__.py
@@ -26,7 +26,7 @@
 from fer.fer import FER
 from fer.classes import Video
 
-__version__ = "19.0.7"
+__version__ = "20.1.0"
 
 __title__ = "fer"
 __description__ = "Facial expression recognition from images"
