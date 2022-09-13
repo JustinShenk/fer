58a00a67b6e5bb4f37cbf0c900f0ea88a1bea810
Tharun-Anand <87030065+Tharun-Anand@users.noreply.github.com>
2022-09-13 21:25:50 +0530

    Update fer.py

--- src/fer/fer.py
+++ src/fer/fer.py
@@ -189,7 +189,7 @@ class FER(object):
         elif self.__face_detector == "mtcnn":
             boxes,probs = self._mtcnn.detect(img)
             faces=[]
-            if any(boxes):
+            if boxes.any():
              for face in boxes:
               faces.append([int(face[0]),int(face[1]),int(face[2])-int(face[0]),int(face[3])-int(face[1])])
  
